d266988d128373c15e8162e060ac093e3d40c75f
Abhilitcode <khaleabhishek351@gmail.com>
2026-06-10 21:55:09 +0530

    added palindrome number code

---
+++ 01_basic_maths/palindrome.py
@@ -0,0 +1,26 @@
+#appraoch 1 math appraoch space-: o(1) time-: o(lognbase10)
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0 or (x%10==0 and x!=0):
+            return False
+        
+        rev = 0
+        while x>rev:
+            last_digit = x%10 
+            rev = rev*10 + last_digit 
+            x = x//10 
+
+        
+        if x==rev or x==rev//10:
+            return True 
+        
+        return False 
+    
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.isPalindrome(121))
+    print(obj.isPalindrome(-121))
+    print(obj.isPalindrome(10))
+    print(obj.isPalindrome(1331))
+    print(obj.isPalindrome(123))
+    print(obj.isPalindrome(0))
