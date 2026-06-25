c0851e45c5900da6d0dace735fc995dedc983458
Abhilitcode <khaleabhishek351@gmail.com>
2026-06-25 22:10:14 +0530

    added majority element edit

--- hashing/majority_element.py
+++ hashing/majority_element.py
@@ -29,46 +29,46 @@ if __name__ == "__main__":
 
 #better appraoch using hashing o(n), o(n)
 
-class Solution():
-    def majority_element(self,nums):
-        n = len(nums)
-        freq = {}
-        for num in nums:
-            if num in freq:
-                freq[num] +=1
-            else:
-                freq[num] =1
+# class Solution():
+#     def majority_element(self,nums):
+#         n = len(nums)
+#         freq = {}
+#         for num in nums:
+#             if num in freq:
+#                 freq[num] +=1
+#             else:
+#                 freq[num] =1
         
-        for key, val in freq.items():
-            if val > n//2:
-                return key
+#         for key, val in freq.items():
+#             if val > n//2:
+#                 return key
             
-if __name__ == "__main__":
-    obj = Solution()
-    nums = list(map(int,input("Enter the numbers:").split()))
-    print(obj.majority_element(nums))
+# if __name__ == "__main__":
+#     obj = Solution()
+#     nums = list(map(int,input("Enter the numbers:").split()))
+#     print(obj.majority_element(nums))
 
 
 
 
-# majority element brute force approach
-class Solution:
-    def majorityElement(self, nums: List[int]):
+# # majority element brute force approach
+# class Solution:
+#     def majorityElement(self, nums: List[int]):
 
-        n = len(nums)
+#         n = len(nums)
 
-        for i in range(n):
-            count = 0
+#         for i in range(n):
+#             count = 0
 
-            for j in range(n):
-                if nums[i] == nums[j]:
-                    count +=1
+#             for j in range(n):
+#                 if nums[i] == nums[j]:
+#                     count +=1
 
-            if count > n//2:
-                return nums[i]
+#             if count > n//2:
+#                 return nums[i]
 
-if __name__ == "__main__":
-    obj = Solution()
-    nums = list(map(int,input("Enter the numbers:").split()))
-    print(obj.majorityElement(nums))
-    print(obj.majorityElement(nums=[3,2,3]))
+# if __name__ == "__main__":
+#     obj = Solution()
+#     nums = list(map(int,input("Enter the numbers:").split()))
+#     print(obj.majorityElement(nums))
+#     print(obj.majorityElement(nums=[3,2,3]))
