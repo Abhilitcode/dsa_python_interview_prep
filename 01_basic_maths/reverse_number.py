72e44b32f23de06af5592dbf7ee0173e098d809d
Abhilitcode <khaleabhishek351@gmail.com>
2026-07-19 17:49:36 +0530

    tried reverse num again

--- 01_basic_maths/reverse_number.py
+++ 01_basic_maths/reverse_number.py
@@ -1,7 +1,7 @@
 class Solution:
     def reverse(self, x: int) -> int:
         #another appraoch 2
-        if (x>=0):
+        if x>=0:
             rev = int(str(x)[::-1])
         else:
             rev = -(int(str(abs(x))[::-1]))
