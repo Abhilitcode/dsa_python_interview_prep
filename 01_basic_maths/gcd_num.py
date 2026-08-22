fd0fcf345f0d9d1de836728f5b4a61efd00fc6b0
Abhilitcode <khaleabhishek351@gmail.com>
2026-08-22 10:45:52 +0530

    added comments to gcd

--- 01_basic_maths/gcd_num.py
+++ 01_basic_maths/gcd_num.py
@@ -1,6 +1,6 @@
 #Appraoch 2 . implement euclidean algorithm space-: o(1) time-: o(logn)
 #gcd of smallest and largest number -> finding. not the entire array list
-
+# understand why this condition in while loop scroll down. 
 from typing import List
 class Solution:
     def findGCD(self, nums: List[int]) -> int:
@@ -27,3 +27,9 @@ if __name__ == "__main__":
     obj = Solution()
     print(obj.findGCD([2,5,6,9,10]))
     print(obj.findGCD([7,5,6,8,3]))
+
+# This is the Euclidean Algorithm for finding the
+# Greatest Common Divisor (GCD) of two numbers.
+# The while min_num != 0: loop is used because we don't know in 
+# advance how many division steps it will take to reduce the remainder to zero.
+# this is same as division method we did in schools
